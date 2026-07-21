a28314f3a15fc3c253e4938106b8fa18379dad79
itd <itd220222@gmail.com>
2026-07-21 12:17:08 +0300

    run_app.sh: change to run with /bin/python

--- run_app.sh
+++ run_app.sh
@@ -7,5 +7,5 @@ if command -v pdfmanifest >/dev/null 2>&1; then
     pdfmanifest "$@"
 else
     # Not installed -> run straight from src/ without installing.
-    PYTHONPATH="src" python3 -m pdfmanifest.main "$@"
+    PYTHONPATH="src" /bin/python3 -m pdfmanifest.main "$@"
 fi
